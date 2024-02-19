97286aa82e64fe23a4b801a82c703c741431db38
1160 <1160@fundacc.local>
2024-02-19 14:56:53 -0300

    Continuação do Curso

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 //alert('Olá mundo');
 
-document.querySelector('.tecla_pom');
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[4].onclick = tocaSomSplash;
+
+let contador = 0;
+
+while (contador < 9) {
+    listaDeTeclas[0].onclick = tocaSomSplash;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+};
