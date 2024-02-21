8c74e01240d49e8c49790624e56c1ccf9a41da10
rafael Campos <rafael.campos.m@hotmail.com>
2024-02-21 14:46:44 -0300

    inclusão funções anônimas

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-
-
 function tocaSom(idElementAudio) {
     document.querySelector(idElementAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = tocaSom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]
+    //template string
+    const idAudio = `#som_${instrumento}`;
 
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+
+    };
     contador = contador + 1;
-
     console.log(contador);
 
-};
+}
