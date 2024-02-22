a49b69056c240fdb7ee61015515a7f6b288a66bb
rafael Campos <rafael.campos.m@hotmail.com>
2024-02-22 13:44:58 -0300

    Finalização do curso, projeto final

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+#Programa em Javascript onde é possivel ouvir teclas de instrumentos músicais ao clicar no botão
--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom(idElementAudio) {
-  document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName == 'audio') {
+
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado')
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla"); //esta const é um arr
@@ -7,28 +16,28 @@ const listaDeTeclas = document.querySelectorAll(".tecla"); //esta const é um ar
 let contador = 0;
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
-  const tecla = listaDeTeclas[i];
-  const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`; //template string
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-  console.log(idAudio);
+    console.log(idAudio);
 
-  tecla.onclick = function () {
-    tocaSom(idAudio);
-  };
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
 
-  tecla.onkeydown = function (evento) {
-    //evento, quando a tecla é pressionada
+    tecla.onkeydown = function (evento) {
+        //evento, quando a tecla é pressionada
 
-    console.log(evento.code == 'Space'); //printa a tecla que foi digitada
+        //console.log(evento.code === 'Space'); //printa a tecla que foi digitada
 
-    if (evento.code == "Space") {
-      tecla.classList.add("ativa");
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+        tecla.onkeyup = function () {
+            //quando solta a tecla
+            tecla.classList.remove('ativa');
+        };
     }
-  };
-
-  tecla.onkeyup = function () {
-    //quando solta a tecla
-    tecla.classList.remove("ativa");
-  };
-}
+};
